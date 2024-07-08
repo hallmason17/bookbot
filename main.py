a924397819dffdebfff99dd9c9413edfca1cb991
hallmason17 <hallmason17@gmail.com>
2024-07-07 20:34:11 -0400

    added report of words and char coutns

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def read_book(path_to_book):
     with open(path_to_book) as f:
         contents = f.read()
@@ -17,10 +16,31 @@ def count_chars(book):
             dict[lowered] = 1
     return dict
     
+def sort_on(dict):
+    return dict["num"]
+
+def dict_to_sorted_list(dict):
+    dict_list = []
+    for char in dict:
+        dict_list.append({"char": char, "num": dict[char]})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def print_report(path, word_count, char_dict):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    dict_list = dict_to_sorted_list(char_dict)
+    for item in dict_list:
+        if item["char"].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
 
 def main():
-    contents = read_book("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    contents = read_book(path)
+    print(count_words(contents))
     print(count_chars(contents))
+    print_report(path, count_words(contents), count_chars(contents))
 
 if __name__=="__main__":
     main()
